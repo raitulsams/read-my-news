753c026796e5f5175d660ce1851d2fdfa1069118
raitulsams <raitulsams202@gmail.com>
2025-10-15 04:56:39 +0200

    social links linking

--- src/components/homelayout/SocialLinks.jsx
+++ src/components/homelayout/SocialLinks.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { FaFacebookF } from "react-icons/fa6";
 import { IoLogoInstagram } from "react-icons/io5";
 import { BsTwitterX } from "react-icons/bs";
@@ -9,7 +8,8 @@ const SocialLinks = () => {
             <div className="flex flex-col border-2 border-red-100">
                 <button className="btn btn-ghost justify-start">
                     <FaFacebookF size={26} className='p-1.5 bg-base-300 rounded-full '></FaFacebookF>
-                    <span>Facebook</span>
+                    <span>Facebook
+                    </span>
                 </button>
                 <button className="btn btn-ghost justify-start ">
                     <IoLogoInstagram size={26} className='p-1.5 bg-base-300 rounded-full'></IoLogoInstagram>
